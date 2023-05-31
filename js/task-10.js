ac948aa47bce65be8a56ea84cbf17d97384153c1
ihorkoi <koleev.igor@gmail.com>
2023-05-31 13:32:13 +0300

    update
    delete excessive css and js variables

--- js/task-10.js
+++ js/task-10.js
@@ -12,9 +12,8 @@ function getRandomHexColor() {
 function createBoxes(amount) {
   const divCollection = [];
   for (let i = 0; i < amount; i++) {
-    const divWidth = (30 + (i * 10)) + 'px';
-    const divHeight = (30 + (i * 10)) + 'px';
-    const newDiv = `<div style=width:${divWidth};height:${divHeight};background-color:${getRandomHexColor()} ></div>`
+    const divSize = (30 + (i * 10)) + 'px';
+    const newDiv = `<div style=width:${divSize};height:${divSize};background-color:${getRandomHexColor()} ></div>`
     divCollection.push(newDiv);
   }
   return divCollection;
