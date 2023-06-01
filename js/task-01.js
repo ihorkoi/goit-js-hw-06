771ae1172c60e61dfb25cc4a4edc402e0e38f4a5
ihorkoi <koleev.igor@gmail.com>
2023-06-01 09:51:15 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ function categoryCounter() {
     const liRef = document.querySelectorAll('li.item')
     console.log('Number of categories: ', liRef.length)
     liRef.forEach(item =>{
-        console.log('Category:', item.querySelector('h2').textContent)
-        console.log('Elements:', item.querySelectorAll('li').length)})
+        console.log('Category:', item.firstElementChild.textContent)
+        console.log('Elements:', item.lastElementChild.childElementCount)})
 }
 categoryCounter()
